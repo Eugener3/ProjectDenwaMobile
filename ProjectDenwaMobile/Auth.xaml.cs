f613e3bd729e6b73a0bc48419512141c7050449d
Eugener3 <musi-masi@mail.ru>
2023-04-25 20:38:36 +0700

    Add project files.

---
+++ ProjectDenwaMobile/Auth.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectDenwaMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Auth : ContentPage
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+        private async void AuthButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+        private async void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+
+        private async void HomeButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ ProjectDenwaMobile/SignUp.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectDenwaMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+        private async void HomeButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        private async void SignInButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Auth());
+        }
+    }
+}
